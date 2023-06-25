117f72ac51ece6c6bed778527905bdd79a37f927
orhan arda duman <orhanardaduman@outlook.com>
2023-06-25 18:21:21 +0300

    minor fix 2

--- bricks/new_app/__brick__/lib/helpers/extansions/string_extensions.dart
+++ bricks/new_app/__brick__/lib/helpers/extansions/string_extensions.dart
@@ -1,6 +1,8 @@
+import '../constants/app_constants.dart';
+
 extension ImagePath on String {
   String get toSvg => "assets/icons/$this.svg";
 
-  String get toNetwork => "/$this";
-  String get toApi => "/$this";
+  String get toNetwork => "${AppConstants.instance.baseUrlForImage}}/$this";
+  String get toApi => "${AppConstants.instance.baseUrl}/$this";
 }
